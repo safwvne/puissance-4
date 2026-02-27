4aaad56de81f86da1d0eef4ff58649158a13abd3
SAFFFFFFF7 <saf.alg699@gmail.com>
2026-02-27 08:26:58 +0100

    first commit

---
+++ src/main/java/com/devops/puissance4/Launcher.java
@@ -0,0 +1,9 @@
+package com.devops.puissance4;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
