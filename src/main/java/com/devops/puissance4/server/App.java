a50d8823979702167162923f922c6184d67cead6
DidiSmash <killiantonione@gmail.com>
2026-03-25 15:23:53 +0100

    fc

---
+++ src/main/java/com/devops/puissance4/server/App.java
@@ -0,0 +1,13 @@
+package com.devops.puissance4.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
